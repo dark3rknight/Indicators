3cd27aec66c737860105ccbce08c4be444a8a33b
dark3rknight <sarthak11098@iiitd.ac.in>
2017-05-26 09:57:14 +0530

    Moduling

---
+++ Exponential_Moving_Averages.py
@@ -0,0 +1,21 @@
+import math
+
+def sma(data, window):
+        if len(data) < window:
+            return None
+        return sum(data[-window:]) / float(window)
+
+def calc_ema(data, window):
+    if len(data) <= 2 * window:
+        return sma(data[-window:],window)
+    multiplier = 2.0 / (window + 1)
+    current_ema = sma(data[-window*2:-window], window)
+    for value in data[-window:]:
+        current_ema = (multiplier * value) + ((1 - multiplier) * current_ema)
+    return current_ema
+
+def get_all_EMAs(data, window):
+    EMA = [None] * ((window))
+    for i in range(window, len(data)):
+        EMA.append(calc_ema(data[:i], window))
+    return EMA
---
+++ dataPlot.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+def multiAxis_LabeledPlot(vals1, label1, vals2 = None, label2 = None, vals3 = None, label3 = None, vals4 = None, label4 = None):
+    plt.close()
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.plot(vals1, color = 'darkred', linewidth = 0.6, label = label1)
+    if vals2 != None:
+        ax1.plot(vals2, color = 'blue', linewidth = 0.6, label = label2)
+    if vals3 != None:
+       ax1.plot(vals3, color = 'green', linewidth = 0.6, label = label3)
+    if vals4 != None:
+        ax4 = ax1.twinx()
+        ax4.plot(vals4, color = 'salmon', linewidth = 0.6, label = label4)
+    ax1.legend()
+    plt.show()
+
+def multiAxisplot(vals1, vals2 = None, vals3 = None, vals4 = None):
+    plt.close()
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.plot(vals1, color = 'darkred', linewidth = 0.6)
+    if vals2 != None:
+        ax1.plot(vals2, color = 'blue', linewidth = 0.6)
+    if vals3 != None:
+       ax1.plot(vals3, color = 'green', linewidth = 0.6)
+    if vals4 != None:
+        ax4 = ax1.twinx()
+        ax4.plot(vals4, color = 'salmon', linewidth = 0.6)
+    plt.show()
